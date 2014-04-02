2c5027ca5a93472a5971ff02b579aa2c8d066e37
Matthew Bates <Matt@Shockoe.com>
2014-04-01 22:27:02 -0400

    Testing the update site

--- Update.php
+++ Update.php
@@ -4,12 +4,15 @@
 			function pullFromBranch(){
 				var branch = document.getElementById("branchName").value;
 				var log = document.getElementById("log").value;
+				alert(1);
 				document.getElementById("branchName").innerHTML += branch;
 				var xmlHttp = new XMLHttpRequest();
+				alert(2);
 				xmlHttp.open( "GET", "PullGraphCannon.php?branch="+branch, false );
 				xmlHttp.send( null );
+				alert(3);
 				log.innerHTML = xmlHttp.responseText;
-				alert(1);
+				alert(4);
 			}
 		</script>
 		<div>Pull from branch:</div>
