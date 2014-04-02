25364526fda983c0cbbefabcb1dc3f381f34fb86
Matthew Bates <Matt@Shockoe.com>
2014-04-01 22:13:02 -0400

    Testing the update site

--- Update.php
+++ Update.php
@@ -1,13 +1,11 @@
 <html>
 	<head>
-		<div>
-			<div>This is a test</div>
-			<select style="float:left">
-				<option>test1</option>
-				<option>test2</option>
-				<option>test3</option>
-			</select>
-		</div>
+		<script>
+			function pullFromBranch(){
+				var branch = document.getElementById("branchName").value;
+				document.getElementById("branchName").innerHTML += branch;
+			}
+		</script>
 		<div>Pull from branch:</div>
 		<div>
 			<form>
@@ -15,11 +13,9 @@
 				<input type="text" onclick="pullFromBranch();" value="master" id="branchName">
 			</form>
 		</div>
+		<div>
+			<h1>Log:</h1>
+			<div id="log"/>
+		</div>
 	</head>
-	<script>
-		function pullFromBranch(){
-			var branch = document.getElementById("BranchName").value;
-			alert(branch);
-		}
-	</script>
 </html>
