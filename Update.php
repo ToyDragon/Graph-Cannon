a74d22e6698378136ff913505b5b006c31eab451
Matthew Bates <Matt@Shockoe.com>
2014-04-01 22:04:54 -0400

    Testing branch field

--- Update.php
+++ Update.php
@@ -5,8 +5,18 @@
 			<option>test2</option>
 			<option>test3</option>
 		</select>
-		<form>
-			<input type="text" onclick="pullFromBranch" value="master">
-		</form>
+		<div>Pull from branch:</div>
+		<div>
+			Branch: 
+			<form>
+				<input type="text" onclick="pullFromBranch();" value="master" id="branchName">
+			</form>
+		</div>
 	</head>
+	<script>
+		function pullFromBranch(){
+			var branch = document.getElementById("BranchName").value;
+			alert(branch);
+		}
+	</script>
 </html>
