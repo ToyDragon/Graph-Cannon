e14c24182d3cf0306b2a8fe0f8f29b0f373aa9f9
Matthew Bates <Matt@Shockoe.com>
2014-04-01 22:07:33 -0400

    Testing the update site

--- Update.php
+++ Update.php
@@ -1,15 +1,18 @@
 <html>
 	<head>
-		<select>
-			<option>test1</option>
-			<option>test2</option>
-			<option>test3</option>
-		</select>
+		<div>
+			<div>This is a test</div>
+			<select style="float:left">
+				<option>test1</option>
+				<option>test2</option>
+				<option>test3</option>
+			</select>
+		</div>
 		<div>Pull from branch:</div>
 		<div>
 			Branch: 
 			<form>
-				<input type="text" onclick="pullFromBranch();" value="master" id="branchName">
+				<input type="text" onclick="pullFromBranch();" value="master" id="branchName" style="float:left">
 			</form>
 		</div>
 	</head>
